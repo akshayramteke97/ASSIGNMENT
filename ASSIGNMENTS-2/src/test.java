1effc31fef6cf95a07974d5b1751162a42e1eb76
Shashank <Shashank@DESKTOP-O4E7A5M>
2022-12-10 15:07:34 +0530

    another commit

---
+++ ASSIGNMENTS-2/src/test.java
@@ -0,0 +1,9 @@
+
+public class test {
+public static void main(String[] args) {
+	StringBuilder s1= new StringBuilder("Shashu");
+	StringBuilder s2 = new StringBuilder ("Shashu");
+	System.out.println(s1==s2);
+	System.out.println(s1.equals(s2));
+}
+}
